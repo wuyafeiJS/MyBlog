06adaf915ee73893eed3d4581022b140f2224ec0
wuyafei <wuyf@mysoft.com.cn>
2018-12-05 10:03:56 +0800

    feat

---
+++ gatsby-config.js
@@ -0,0 +1,73 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Trace of Front-end',
+    author: 'Yafei Wu',
+    description: '留下自己前端历程的足迹',
+    siteUrl: 'http://wuhub.top/',
+  },
+  pathPrefix: '/',
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              inlineCodeMarker: '÷',
+            },
+          },
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-130227707-1`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Overreacted`,
+        short_name: `Overreacted`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#ffa7c4`,
+        display: `minimal-ui`,
+        icon: `src/assets/icon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography',
+      },
+    },
+  ],
+}
